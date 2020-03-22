8d2e51122ed2b34acaa471d230e4595122a456cb
seanr89 <srafferty89@gmail.com>
2020-03-22 19:24:41 +0000

    Working on moving moving exception logic to utilities

---
+++ src/Services/Utilities/Middleware/ErrorDetails.cs
@@ -0,0 +1,23 @@
+
+
+using System.Diagnostics.CodeAnalysis;
+using Newtonsoft.Json;
+
+namespace Utilities
+{
+    [ExcludeFromCodeCoverage]
+    /// <summary>
+    /// Custom model for handling Global Error Middleware
+    /// </summary>
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ src/Services/Utilities/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,45 @@
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Utilities.Middleware
+{
+    [ExcludeFromCodeCoverage]
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError($"Something went wrong: {ex}");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            return context.Response.WriteAsync(new ErrorDetails()
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "Internal Server Error from the custom middleware."
+            }.ToString());
+        }
+    }
+}
